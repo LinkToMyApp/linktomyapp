6d200080e841730ff33f6c389e7838ac50794871
boris charp <boris.charpentier@gmail.com>
2013-10-19 18:58:38 +0200

    table + start charts

---
+++ public/app/scripts/controllers/leftMenu.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('linkToMyApp')
+  .controller('LeftmenuCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.referers = ["",""];
+    $scope.app_icon = "http://a4.mzstatic.com/us/r30/Purple/v4/ff/42/bb/ff42bbec-132d-3722-6218-bec51eb43e5e/mzl.clfucble.175x175-75.jpg";
+    $scope.app_link = "http://www.google.com";
+  });
--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -1,132 +1,307 @@
 'use strict';
 
-angular.module('linkToMyApp').controller('MainCtrl', function ($scope) {
-
-    var chart1 = {};
-    chart1.type = "AreaChart";
-    chart1.displayed = false;
-    chart1.cssStyle = "height:600px; width:100%;";
-    chart1.data = {"cols": [
-        {id: "month", label: "Month", type: "string"},
-        {id: "laptop-id", label: "Laptop", type: "number"},
-        {id: "desktop-id", label: "Desktop", type: "number"},
-        {id: "server-id", label: "Server", type: "number"},
-        {id: "cost-id", label: "Shipping", type: "number"}
-    ], "rows": [
-        {c: [
-            {v: "January"},
-            {v: 1, f: "42 items"},
-            {v: 1, f: "Ony 12 items"},
-            {v: 1, f: "7 servers"},
-            {v: 1}
-        ]},
-        {c: [
-            {v: "February"},
-            {v: 13},
-            {v: 1, f: "1 unit (Out of stock this month)"},
-            {v: 12},
-            {v: 2}
-        ]},
-        {c: [
-            {v: "March"},
-            {v: 24},
-            {v: 5},
-            {v: 11},
-            {v: 6}
-
-        ]}
-    ]};
-
-    chart1.options = {
-        "title": "Sales per month",
-        "isStacked": "true",
-        "fill": 20,
-        "displayExactValues": true,
-        "vAxis": {
-            "title": "Sales unit", "gridlines": {"count": 10}
-        },
-        "hAxis": {
-            "title": "Date"
-        }
-    };
-    
-
-    var formatCollection = [
-      {
-        name: "color",
-        format: [
-          {
-            columnNum: 4,
-            formats: [
-              {
-                from: 0,
-                to: 3,
-                color: "white",
-                bgcolor: "red"
-              },
-              {
-                from: 3,
-                to: 5,
-                color: "white",
-                fromBgColor: "red",
-                toBgColor: "blue"
-              },
-              {
-                from: 6,
-                to: null,
-                color: "black",
-                bgcolor: "#33ff33"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        name: "arrow",
-        checked:false,
-        format: [
-          {
-            columnNum: 1,
-            base: 19
-          }
-        ]
-      },
-      {
-        name: "date",
-        format: [
-          {
-            columnNum: 5,
-            formatType: 'long'
-          }
-        ]
-      },
-      {
-        name: "number",
-        format: [
-          {
-            columnNum: 4,
-            prefix: '$'
-          }
-        ]
-      },
-      {
-        name: "bar",
-        format: [
-          {
-            columnNum: 1,
-            width: 100
-          }
-        ]
-      }
-    ];
-
-    chart1.formatters = {};
-
-    $scope.chart = chart1;
-    $scope.chart2 = chart1;
+angular.module('linkToMyApp').controller('MainCtrl', function ($scope, $http) {
 
     $scope.chartReady = function (){
 
     };
+
+    function createTableData(){
+
+        var tableData = {
+                      "type": "Table",
+                      "displayed": true,
+                      "cssStyle": "height:100px; width:100%; margin-top: 20px;",
+                      "data": {
+                        "cols": [],
+                        "rows": [],
+                      },
+                      "options": {
+                        "title": "Installs & Clicks",
+                        "isStacked": "true",
+                        "fill": 20,
+                        "displayExactValues": true
+                        },
+                      "formatters": {}
+                    };
+
+            var cols = [{
+                            "id": "name",
+                            "label": "Name",
+                            "type": "string"
+                          },
+                          {
+                            "id": "clicks",
+                            "label": "Clicks",
+                            "type": "number"
+                          },
+                          {
+                            "id": "installs",
+                            "label": "Installs",
+                            "type": "number"
+                          }];
+
+            var rows = [];
+
+            var length = $scope.referers.length,element = null;
+            for (var i = 0; i < length; i++) {
+              element = $scope.referers[i];
+
+              var object = {
+                            "c": [
+                              {
+                                "f": element.name
+                              },
+                              {
+                                "v": element.clicks
+                              },
+                              {
+                                "v": element.installs
+                              }
+                            ]
+                          };
+                rows.push(object);
+            }
+
+            tableData.data.cols = cols;
+            tableData.data.rows = rows;
+
+            return tableData;
+        }
+
+    function createGraphData(){
+
+        var graphData = {
+                      "type": "LineChart",
+                      "displayed": true,
+                      "cssStyle": "height:600px; width:100%;",
+                      "data": {
+                        "cols": [],
+                        "rows": []
+                      },
+                      "options": {
+                        "title": "Sales per month",
+                        "isStacked": "true",
+                        "fill": 20,
+                        "displayExactValues": true,
+                        "vAxis": {
+                          "title": "Sales unit",
+                          "gridlines": {
+                            "count": 10
+                          }
+                        },
+                        "hAxis": {
+                          "title": "Date"
+                        }
+                      },
+                      "formatters": {}
+                    };
+
+            var cols = [
+                          {
+                            "id": "month",
+                            "label": "Month",
+                            "type": "string"
+                          },
+                          {
+                            "id": "laptop-id",
+                            "label": "Laptop",
+                            "type": "number"
+                          },
+                          {
+                            "id": "desktop-id",
+                            "label": "Desktop",
+                            "type": "number"
+                          },
+                          {
+                            "id": "server-id",
+                            "label": "Server",
+                            "type": "number"
+                          },
+                          {
+                            "id": "cost-id",
+                            "label": "Shipping",
+                            "type": "number"
+                          }];
+
+    var rows = [
+                  {
+                    "c": [
+                      {
+                        "v": "January"
+                      },
+                      {
+                        "v": 19,
+                        "f": "42 items"
+                      },
+                      {
+                        "v": 12,
+                        "f": "Ony 12 items"
+                      },
+                      {
+                        "v": 7,
+                        "f": "7 servers"
+                      },
+                      {
+                        "v": 4
+                      }
+                    ]
+                  },
+                  {
+                    "c": [
+                      {
+                        "v": "February"
+                      },
+                      {
+                        "v": 13
+                      },
+                      {
+                        "v": 1,
+                        "f": "1 unit (Out of stock this month)"
+                      },
+                      {
+                        "v": 12
+                      },
+                      {
+                        "v": 2
+                      }
+                    ]
+                  },
+                  {
+                    "c": [
+                      {
+                        "v": "March"
+                      },
+                      {
+                        "v": 24
+                      },
+                      {
+                        "v": 5
+                      },
+                      {
+                        "v": 11
+                      },
+                      {
+                        "v": 6
+                      }
+                    ]
+                  }
+                ];
+
+            /*var length = $scope.referers.length,element = null;
+            for (var i = 0; i < length; i++) {
+              element = $scope.referers[i];
+
+              var object = {
+                            "c": [
+                              {
+                                "f": element.name
+                              },
+                              {
+                                "v": element.clicks
+                              },
+                              {
+                                "v": element.installs
+                              }
+                            ]
+                          };
+                rows.push(object);
+            }*/
+
+            graphData.data.cols = cols;
+            graphData.data.rows = rows;
+
+            return graphData;
+        }
+    
+
+    /**************************** Dummy Data **************************/
+
+        $scope.referers = [
+        {
+            "name": "toto",
+            "clicks": 10,
+            "installs": 8
+        },
+        {
+            "name": "titi",
+            "clicks": 20,
+            "installs": 5
+        }
+        ];
+
+
+        $scope.graphData = [
+        {
+            "date": "10/10",
+            "values": [
+                {
+                    "name": "toto",
+                    "value": 2
+                },
+                {
+                    "name": "titi",
+                    "value": 1
+                }
+            ]
+        },
+        {
+            "date": "11/10",
+            "values": [
+                {
+                    "name": "toto",
+                    "value": 1
+                },
+                {
+                    "name": "titi",
+                    "value": 3
+                }
+            ]
+        },
+        {
+            "date": "12/10",
+            "values": [
+                {
+                    "name": "toto",
+                    "value": 1
+                },
+                {
+                    "name": "titi",
+                    "value": 3
+                }
+            ]
+        },
+        {
+            "date": "13/10",
+            "values": [
+                {
+                    "name": "toto",
+                    "value": 2
+                },
+                {
+                    "name": "titi",
+                    "value": 2
+                }
+            ]
+        },
+        {
+            "date": "14/10",
+            "values": [
+                {
+                    "name": "toto",
+                    "value": 3
+                },
+                {
+                    "name": "titi",
+                    "value": 1
+                }
+            ]
+        }
+    ];
+
+    /************************** Creation **************************/
+
+    $scope.table = createTableData();
+    $scope.graph = createGraphData();
 
   });
