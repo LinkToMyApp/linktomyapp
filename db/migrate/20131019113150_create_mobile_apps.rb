89abad4de6cf4873c60245ecbe8dc8b24f4b2c6e
vincent daubry <vdaubry@gmail.com>
2013-10-19 13:42:10 +0200

    conf js

--- db/migrate/20131019113150_create_mobile_apps.rb
+++ db/migrate/20131019113150_create_mobile_apps.rb
@@ -1,11 +1,12 @@
 class CreateMobileApps < ActiveRecord::Migration
   def change
     create_table :mobile_apps do |t|
-      t.string, :name
-      t.string, :itunes_url
-      t.string :icon_url
+      t.string :name,		:null => false
+      t.string :itunes_url,	:null => false
+      t.string :icon_url,	:null => false
 
       t.timestamps
     end
   end
 end
+	
