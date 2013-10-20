381c958dc0af8f665d2bc120c42a627cc52f6962
vincent daubry <vdaubry@gmail.com>
2013-10-20 13:40:01 +0200

    correction

--- app/controllers/api/app_links_controller.rb
+++ app/controllers/api/app_links_controller.rb
@@ -39,7 +39,7 @@ class Api::AppLinksController < ApplicationController
 		else
 			link_click = link_clicks.first
 			link_click.update_attributes(:installed => true)
-			link_click.app_link.increment_counter(:installs_count, 5)
+			link_click.app_link.installs_count += 1
 			link_click.app_link.save
 			link_click_id=link_click.id
 		end
