d64e99fb02b83e99bf0476ccd26909ef7fe74b89
vincent daubry <vdaubry@gmail.com>
2013-10-20 11:45:05 +0200

    ajout des endpoints app installed et event

--- spec/controllers/api/app_links_controller_spec.rb
+++ spec/controllers/api/app_links_controller_spec.rb
@@ -93,11 +93,10 @@ describe Api::AppLinksController do
 		end
 	end
 
-	describe "event" do
+	describe "app_installed" do
 		before(:each) do
 			@ip_adress = "1.1.1.1"
-	      	@controller.request.stubs(:remote_ip).returns(@ip_adress)
-	      	@callback_url = "http://linktomyapp.herokuapp.com/"
+			@controller.request.stubs(:remote_ip).returns(@ip_adress)
 		end
 
 		let(:mobile_app) { FactoryGirl.create(:mobile_app, :id => 12, :callback_url => @callback_url) }
@@ -107,43 +106,21 @@ describe Api::AppLinksController do
 		context "link was clicked" do
 			context "same IP address" do
 				it "returns link click id" do
-					stub_request(:any, @callback_url)
 					FactoryGirl.create(:link_click, :id => 123, :installed => false, :ip_adress => @ip_adress, :app_link => app_link)
 
-					post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
+					post :app_installed, :app_id => mobile_app.id, :udid => "a_udid", :format => :json
 
 					resp = JSON.parse(response.body)
 					resp["link_click_id"].should eq(123)
 				end
 
 				it "updates link click status" do
-					stub_request(:any, @callback_url)
+					#stub_request(:any, @callback_url)
 					link_click = FactoryGirl.create(:link_click, :id => 123, :installed => false, :ip_adress => @ip_adress, :app_link => app_link)
 
-					post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
+					post :app_installed, :app_id => mobile_app.id, :udid => "a_udid", :format => :json
 
 					link_click.reload.installed.should == true
-				end
-
-				context "has a callback_url" do
-					it "sends event to callback url" do
-						stub_request(:post, "http://linktomyapp.herokuapp.com/").to_return(:body => "abc")
-
-						FactoryGirl.create(:link_click, :id => 123, :installed => false, :ip_adress => @ip_adress, :app_link => app_link)
-
-						post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
-
-						WebMock.should have_requested(:post, @callback_url).with(:body => { 'udid' => "a_udid", :event => "an_event", :referal => "a_referal"}).once
-					end
-				end
-				context "dont have a callback_url" do
-					it "ignores callback" do
-						mobile_app.update_attributes(:callback_url => nil)
-
-						FactoryGirl.create(:link_click, :id => 123, :installed => false, :ip_adress => @ip_adress, :app_link => app_link)
-
-						post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
-					end
 				end
 			end
 
@@ -152,7 +129,7 @@ describe Api::AppLinksController do
 					SecureRandom.stubs(:hex).returns(321)
 					FactoryGirl.create(:link_click, :id => 123, :installed => false, :ip_adress => "2.2.2.2", :app_link => app_link)
 
-					post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
+					post :app_installed, :app_id => mobile_app.id, :udid => "a_udid", :format => :json
 
 					resp = JSON.parse(response.body)
 					resp["link_click_id"].should eq(321)
@@ -161,7 +138,7 @@ describe Api::AppLinksController do
 				it "doesn't update the link click status" do
 					link_click = FactoryGirl.create(:link_click, :id => 123, :installed => false, :ip_adress => "2.2.2.2", :app_link => app_link)
 
-					post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
+					post :app_installed, :app_id => mobile_app.id, :udid => "a_udid", :format => :json
 
 					link_click.reload.installed.should == false
 				end
@@ -172,11 +149,47 @@ describe Api::AppLinksController do
 			it "returns random unique id" do
 				SecureRandom.stubs(:hex).returns(321)
 
-				post :event, :app_id => mobile_app.id, :udid => "a_udid", :event => "an_event", :format => :json
+				post :app_installed, :app_id => mobile_app.id, :udid => "a_udid", :format => :json
 
 				resp = JSON.parse(response.body)
 				resp["link_click_id"].should eq(321)
 			end
 		end
+	end
+
+
+	describe "event" do
+		before(:each) do
+			@callback_url = "http://linktomyapp.herokuapp.com/"
+		end
+
+		let(:mobile_app) { FactoryGirl.create(:mobile_app, :id => 12, :callback_url => @callback_url) }
+
+		let(:app_link) { FactoryGirl.create(:app_link, :mobile_app => mobile_app, :referal => "a_referal") }
+
+		context "link_click exist" do
+			context "has a callback_url" do
+				it "sends app_installed to callback url" do
+					stub_request(:post, "http://linktomyapp.herokuapp.com/").to_return(:body => "abc")
+
+					link_click = FactoryGirl.create(:link_click, :id => 123, :installed => false, :app_link => app_link)
+
+					post :event, :app_id => mobile_app.id, :id => link_click.id, :event => "an_event", :format => :json
+
+					WebMock.should have_requested(:post, @callback_url).with(:body => {:event => "an_event", :referal => "a_referal"}).once
+				end
+			end
+			context "dont have a callback_url" do
+				it "ignores callback" do
+					mobile_app.update_attributes(:callback_url => nil)
+
+					link_click = FactoryGirl.create(:link_click, :id => 123, :installed => false, :app_link => app_link)
+
+					post :app_installed, :app_id => mobile_app.id, :id => link_click.id, :event => "an_event", :format => :json
+				end
+			end
+		end
+
+		
 	end
 end
